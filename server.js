56aa11270ca0144a6695918c7e46db41d3110915
unknown <asnelson93@gmail.com>
2026-08-27 07:00:51 -0400

    Packages installed. Server file setup

---
+++ server.js
@@ -0,0 +1,27 @@
+const Express = require("express")
+const mongoose = require("mongoose")
+const dotenv =  require("dotenv")
+const dns =  require("dns")
+const app = Express()
+
+app.use(Express.json())
+
+dns.setServers(["1.1.1.1","8.8.8.8"])
+dotenv.config()
+
+//app.use("/students", )
+
+// async function connectDB() {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URL);
+//         console.log("Connected successfully");
+//     } catch (error) {
+//         console.log("Database connection failed:", error.message);
+//     }
+// }
+
+connectDB()
+
+app.listen(3000, ()=>{
+    console.log("Listening to PORT")
+})
